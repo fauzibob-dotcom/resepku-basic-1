ea9052be03a133dd4ca234612a7e71efc5577167
AFauzi <fauzi.bob@gmail.com>
2026-05-06 15:23:56 +0700

    add comment to the script

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const favBtn = document.querySelector('.favorite-icon')
 const loveIcon = favBtn.querySelector('span')
 
-
+//add listener and change love button color 
 favBtn.addEventListener('click', function() {
   loveIcon.classList.toggle('text-red-500')
 })
